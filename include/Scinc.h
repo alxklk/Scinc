637103a485bba4416245617b36d9b351445f0aa1
Alex Kluchikov <klkspa@ukr.net>
2021-02-27 19:18:14 +0200

    Utility for dynamic linking

--- include/Scinc.h
+++ include/Scinc.h
@@ -1,1 +1,41 @@
-/home/klk/wrk/Cumulus/Scrip/Scinc.h
+#include <vector>
+#include "base.h"
+class Scinc
+{
+public:
+	union{
+		struct
+		{
+			int rsp;
+			int rthis;
+			int _glob;
+		};
+		int segs[3];
+	};
+	std::vector<unsigned char>mem;
+	int GetInt(int addr)const
+	{
+		return (*((int*)(&mem[addr])));
+	}
+	int GetChar(int addr) const
+	{
+		return mem[addr];
+	}
+	TFloat GetFloat(int addr) const
+	{
+		return (*((TFloat*)(&mem[addr])));
+	}
+	void PutInt(int addr, int val)
+	{
+		(*((int*)(&mem[addr])))=val;
+	}
+	void PutChar(int addr, char val)
+	{
+		(*((char*)(&mem[addr])))=val;
+	}
+	void PutFloat(int addr, const TFloat& val)
+	{
+		(*((TFloat*)(&mem[addr])))=val;
+	}
+
+};
--- include/base.h
+++ include/base.h
@@ -1,1 +1,48 @@
-/home/klk/wrk/Cumulus/Scrip/base.h
+#pragma once
+
+struct Type
+{
+	static const int Error=-1;
+	static const int Auto=0;
+	static const int Void=1;
+	static const int Char=2;
+	static const int Int=3;
+	static const int Float=4;
+	static const int VoidPtr=5;
+	static const int CharPtr=6;
+	static const int IntPtr=7;
+	static const int FloatPtr=8;
+};
+
+typedef double TFloat;
+//typedef float TFloat;
+
+enum STEP_RESULT
+{
+	SR_UNDEF=0,
+	SR_N_USSUED,
+	SR_INTERRUPT,
+	SR_ERROR,
+	SR_RETURN,
+	SR_CALL_NATIVE,
+	SR_FINISHED
+};
+
+struct SShortOp
+{
+	const char* str;
+	int pri; // priority
+	int opn; // operand number
+	int opra; // 1 if right associativity
+};
+
+struct SKeyWord
+{
+	const char* str;
+	int kwval;
+};
+
+#define XSTR(x) #x
+#define ASTR(x) XSTR(x)
+#define ERT(s) s
+//#define ERT(s) s "@" ASTR(__FILE__) ":" ASTR(__LINE__)
