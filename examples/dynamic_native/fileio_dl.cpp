d6b74df2fa48275d217cb2352a0911ce5e6dc0b9
Alex Kluchikov <klkspa@ukr.net>
2021-03-09 20:17:04 +0200

    DLL export for MSVC

--- examples/dynamic_native/fileio_dl.cpp
+++ examples/dynamic_native/fileio_dl.cpp
@@ -7,11 +7,17 @@ struct FILE_ptr{
 	static const int size_of=sizeof(FILE*);
 };
 
+#ifdef MSVC
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT
+#endif
+
 FILE_ptr fopen_impl(const char* fname, const char* mode){return {fopen(fname, mode)};}
-extern "C" int fopen_wrapper(ScincVM* s, void*){ScincBoundCall(s,fopen_impl);return 0;}
+extern "C" EXPORT int fopen_wrapper(ScincVM* s, void*){ScincBoundCall(s,fopen_impl);return 0;}
 
 int fclose_impl(FILE_ptr f){return fclose(f.f);}
-extern "C" int fclose_wrapper(ScincVM* s, void*){ScincBoundCall(s,fclose_impl);return 0;}
+extern "C" EXPORT int fclose_wrapper(ScincVM* s, void*){ScincBoundCall(s,fclose_impl);return 0;}
 
 int fwrite_impl(char* ptr, int size, int n, FILE_ptr f){return fwrite((void*)ptr, size, n, f.f);};
-extern "C" int fwrite_wrapper(ScincVM* s, void*){ScincBoundCall(s,fwrite_impl);return 0;}
+extern "C" EXPORT int fwrite_wrapper(ScincVM* s, void*){ScincBoundCall(s,fwrite_impl);return 0;}
