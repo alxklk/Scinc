2f363e751b7729e765208d6f4cd68ac74aa6397b
Alex Kluchikov <klkspa@ukr.net>
2021-09-15 11:38:08 +0300

    So called FFT waves

---
+++ examples/graphic/waves.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+#include "graphics.h"
+
+Graph g;
+
+#define M_PI 3.141592654
+
+void wave(float& x, float& y, double T, float i, int octaves)
+{
+	float R=9.;
+	float S=.025;
+	float W=-.1;
+	#define RRRRS R*=.56;S*=1.87;W*=1.05;
+	for(int s=0;s<octaves;s++)
+	{
+		x+=sin(T*W+i*S)*R;
+		y+=cos(T*W+i*S)*R;
+		RRRRS
+	}
+}
+
+int main()
+{
+	g.miterlim(1.0);
+	while(true)
+	{
+		double t=Time();
+		g.rgba32(0xff6080ff);
+		g.FillRT();
+
+		float T=t*25;
+		g.clear();
+		{
+			float x0s[5]={50,190,240,400,540};
+			for(int i=0;i<5;i++)
+			{
+				float x=0;
+				float y=0;
+				float x0=x0s[i];
+				wave(x,y,T-3.,x0,2);
+				g.M(x0+x,240.+y);
+			}
+		}
+		g.fin();
+		g.rgba32(0xff800000);
+		g.width(11.,11.);
+		g.stroke();
+		g.rgba32(0xffff8000);
+		g.width(10.,10.);
+		g.stroke();
+
+		g.clear();
+		g.L(-20,500);
+		for(int i=-20;i<680;i+=5)
+		{
+			float x=0;
+			float y=0;
+			float x0=i;
+			wave(x,y,T,x0,15);
+			g.L(x0+x,240.+y);
+		}
+		g.L(660,500);
+		g.close();
+		g.fin();
+		g.rgba32(0x80006080);
+		g.fill1();
+		g.rgba32(0xc0004080);
+		g.width(1.,1.);
+		g.stroke();
+
+		Present();
+	}
+	return 0;
+}
