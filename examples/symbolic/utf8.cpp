774b1ee80c0af0d0a5c92cc814ac768595aca84b
Alex Kluchikov <klkspa@ukr.net>
2023-07-19 17:52:58 +0300

    Silly UTF-8 decode for 2,3 and 4 byte seqs

--- examples/symbolic/utf8.cpp
+++ examples/symbolic/utf8.cpp
@@ -3,6 +3,7 @@
 
 #ifdef __SCINC__
 	#define const
+	#define unsigned
 #endif
 
 struct SUtf8
@@ -16,24 +17,45 @@ int U8Next(SUtf8& S)
 	const char* s=S.s;
 	int p=S.p;
 
-	int c0=s[p];
+	int c0=(unsigned char)s[p];
 	S.p++;
-	if(c0&0x80)
-	{
-		int c1=s[p+1];
-		c0=c0&0x1f;
-		c1=c1&0x3f;
-		if(c1)
-			S.p++;
-		return ((c0<<6)+c1)&0xffff;
-	}
-	else
+
+	if(c0<0x80)
 	{
 		return int(c0);
 	}
+	else if(c0<0xE0)
+	{
+		int c1=s[p+1];
+		if(!c1)return 0;
+		S.p++;
+		return ((c0&0x1f)<<6)|(c1&0x3f);
+	}
+	else if(c0<0xF0)
+	{
+		int c1=s[p+1];
+		int c2=s[p+2];
+		if(!c1)return 0;
+		if(!c2)return 0;
+		S.p+=2;
+		return ((c0&0x1f)<<12)|((c1&0x3f)<<6)|(c2&0x3f);
+	}
+	else
+	{
+		c0=c0&0x07;
+		int c1=s[p+1];
+		int c2=s[p+2];
+		int c3=s[p+3];
+		if(!c1)return 0;
+		if(!c2)return 0;
+		if(!c3)return 0;
+		S.p+=3;
+		return ((c0&0x1f)<<18)|((c1&0x3f)<<12)|((c2&0x3f)<<6)|(c3&0x3f);
+	}
+	return 0;
 }
 
-void CheckU(const char *s0)
+void CheckUtfError(const char *s0)
 {
 	char s[50];
 	for(int j=0;j<50;j++)
@@ -45,7 +67,7 @@ void CheckU(const char *s0)
 				break;
 		}
 		s[j]=0;
-		printf("%s\n",s);
+		printf("'%s'\n>",s);
 		SUtf8 u;
 		u.p=0;
 		u.s=&s[0];
@@ -63,12 +85,30 @@ void CheckU(const char *s0)
 	}
 }
 
+void CheckUtf(const char *s)
+{
+		printf("'%s'\n>",s);
+		SUtf8 u;
+		u.p=0;
+		u.s=&s[0];
+
+		for(int i=0;i<128;i++)
+		{
+			int c=U8Next(u);
+			if(c==0)
+				break;
+			printf("%lc", c);
+		}
+		printf("\n");
+}
+
 int main()
 {
 #ifndef __SCINC__
 	setlocale(LC_ALL,"");
 #endif
-	CheckU("перевірка");
-	CheckU("ПЕРЕВІРКА");
+	CheckUtf("перевірка");
+	CheckUtf("AöЖ€𝄞 ❤ ➆ 🁂  🃗 ❓");
+	CheckUtf("ПЕРЕВІРКА");
 	return 0;
 }
