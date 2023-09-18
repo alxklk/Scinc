b652ddf6c0738de50d0c9691f37c3942976b188c
Alex Kluchikov <klkspa@ukr.net>
2023-09-18 18:28:17 +0300

    Example with dynamic window creation

---
+++ examples/SYS/window.cpp
@@ -0,0 +1,71 @@
+
+#define G_SCREEN_WIDTH 320
+#define G_SCREEN_HEIGHT 320
+#define G_SCREEN_SCALE 4
+#define G_SCREEN_MODE 1
+
+#define M_PI 3.141592654
+
+#include "graphics.h"
+
+Graph g;
+
+double T;
+
+float fx(float x)
+{
+	return (cos(x*13+T)*300*sin(T*1.1)+cos(x*27.5-T*2.3)*250*sin(T*.9)+sin(x*3.1)*150*sin(T*.7))*.25;
+}
+
+float fy(float x)
+{
+	return (sin(x*13+T)*300*sin(T*1.1)+sin(x*27.5-T*.3)*250*sin(T*.9)+cos(x*3.1)*150*sin(T*.7))*.25;
+}
+
+int main()
+{
+	int win0=SYS::CreateWindow(320,320,2,4,2);
+	SYS::MakeBorderless(win0);
+	int win1=SYS::CreateWindow(320,320,4,1,2);
+	while(true)
+	{
+		g.SetActiveRT(SYS::GetWindowRT(win0));
+		g.rgba32(0xff203040);
+		g.FillRT();
+		T=Time();
+		float a=0;
+		float cx=G_SCREEN_WIDTH/2;
+		float cy =G_SCREEN_HEIGHT/2;
+		float x0=cx+fx(a);
+		float y0=cy+fy(a);
+		for(int i=0;i<2500;i++)
+		{
+			g.rgb(.25+.25*cos(a*13*5),.75+.25*sin(a*13*4),.25+.25*sin(a*13*3));
+			a+=0.005;
+			float x1=cx+fx(a);
+			float y1=cy+fy(a);
+			g.hairline(x0,y0,x1,y1);
+			x0=x1;
+			y0=y1;
+		}
+		SYS::Present(win0);
+		g.SetActiveRT(SYS::GetWindowRT(win1));
+		g.rgba32(0xffa0a020);
+		g.FillRT();
+		g.clear();
+		#define N 800
+		for(int i=0;i<N;i++)
+		{
+			float x=(cos(i*M_PI/N*2.*5+T*1.1)*.8+sin(i*M_PI/N*2.*29-T*0.12)*.2)*160+160;
+			float y=(sin(i*M_PI/N*2.*4+T*.93)*.8+cos(i*M_PI/N*2.*27-T*0.37)*.2)*160+160;
+			g.L(x,y);
+		}
+		g.close();
+		g.fin();
+		g.alpha(1);
+		g.rgba32(0xffa000a0);
+		g.fill2();
+		SYS::Present(win1);
+	}
+	return 0;
+}
