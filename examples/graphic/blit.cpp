4142e42af90439ac5c52393456612a817957dce1
Alex Kluchikov <klkspa@ukr.net>
2024-04-09 02:38:29 +0300

    Blit benchmark

---
+++ examples/graphic/blit.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define G_SCREEN_WIDTH 640
+#define G_SCREEN_HEIGHT 480
+#define G_SCREEN_SCALE 2
+#define G_SCREEN_MODE 1
+#include "graphics.h"
+#include "../ws.h"
+
+Graph g;
+
+int main()
+{
+	int n=0;
+	float t0=Time();
+	SetPresentWait(0);
+	while(true)
+	{
+		n++;
+		if(n%2)
+		{
+			g.rgba32(0xff000000);
+		}
+		else
+		{
+			g.rgba32(0xffffffff);
+		}
+		if((n%60)==0)
+		{
+			printf("fps=%f\n", n/(Time()-t0));
+		}
+		g.FillRT();
+		Present();
+	}
+	return 0;
+}
