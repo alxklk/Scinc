793ef6a9bc527e4a77a5dd2eef91684f6bf102ef
Alex Kluchikov <klkspa@ukr.net>
2024-04-16 11:53:21 +0300

    GUI refactor

--- examples/ui/gui/GUI.h
+++ examples/ui/gui/GUI.h
@@ -40,52 +40,75 @@ bool IsMouseEvent(int eventType)
 #define CT_FRAME 7
 #define CT_LINEEDIT 8
 
-struct SButton;
+#define CS_USERBG 1
 
-typedef int(*PFButtonPressCB)(SButton* p);
+struct SWidget;
 
-struct SButton
+typedef int(*PFWidgetActionCB)(SWidget* p);
+typedef int(*PFWidgetRenderCB)(SWidget* p, int state);
+
+
+struct SWidget
 {
 	int type;
+	int styleBitset;
 	char* text;
 	int x0;
 	int y0;
 	int x1;
 	int y1;
 	int tag;
-	int cur;
-	int* iDest;
-	float* fDest;
+	int i0;
+	int i1;
 	float f0;
 	float f1;
-	PFButtonPressCB CB;
-	SButton& SetCB(PFButtonPressCB newCB)
+	int* iDest;
+	float* fDest;
+	PFWidgetActionCB actCB;
+	PFWidgetRenderCB renderCB;
+	SWidget& SetActionCB(PFWidgetActionCB newCB)
 	{
-		CB=newCB;
+		actCB=newCB;
 		return *this;
 	}
-	SButton& SetTitle(char* title)
+	SWidget& SetRenderCB(PFWidgetRenderCB newCB)
+	{
+		renderCB=newCB;
+		return *this;
+	}
+	SWidget& SetTitle(char* title)
 	{
 		text=title;
 		return *this;
 	}
+	SWidget& RaiseStyleBit(int styleBit)
+	{
+		styleBitset=styleBitset|styleBit;
+		return *this;
+	}
+	SWidget& ClearStyleBit(int styleBit)
+	{
+		styleBitset=styleBitset&(0xffffffff^styleBit);
+		return *this;
+	}
+	
 };
 
 #define MAX_TEXT_LEN 512
 
-int LineTextEvent(SButton& b, SScincEvent& ev)
+int LineTextEvent(SWidget& b, SScincEvent& ev)
 {
-	// TODO: sometimes b.text[b.cur] behaves strangely
+	// TODO: sometimes b.text[b.i0] behaves strangely
 	//printf("Event %c%c%c%c\n",(ev.type&0xff000000)>>24,(ev.type&0xff0000)>>16,(ev.type&0xff00)>>8,(ev.type&0xff));
 	char* text=b.text;
-	int& cur=b.cur;
-	//printf(" Cur %i text before: <%s>\n", cur, text);
+	int& i0=b.i0;
+	//printf(" Cur %i text before: <%s>\n", i0, text);
 	if(ev.type=='CHAR')
 	{
 		if((ev.x>=' ')&&(ev.x<127))
 		{
-			int old=text[cur];
-			for(int i=cur+1;i<MAX_TEXT_LEN;i++)
+			int old=text[i0];
+			for(int i=i0+1;i<MAX_TEXT_LEN;i++)
 			{
 				int oldold=old;
 				old=text[i];
@@ -96,7 +119,7 @@ int LineTextEvent(SButton& b, SScincEvent& ev)
 					break;
 				}
 			}
-			text[cur++]=ev.x;
+			text[i0++]=ev.x;
 		}
 	}
 	if(ev.type=='KBDN')
@@ -104,22 +127,22 @@ int LineTextEvent(SButton& b, SScincEvent& ev)
 		//printf("key %i\n", ev.x);
 		if(ev.x==4002)
 		{
-			int c=text[cur];
-			//printf("Char under cursor %i %i %i\n", cur, c);
-			if(cur>0)cur--;
+			int c=text[i0];
+			//printf("Char under cursor %i %i %i\n", i0, c);
+			if(i0>0)i0--;
 		}
 		if(ev.x==4003)
 		{
-			int c=text[cur];
-			//printf("Char under cursor %i %i\n", cur, c);
-			if(text[cur])cur++;
+			int c=text[i0];
+			//printf("Char under cursor %i %i\n", i0, c);
+			if(text[i0])i0++;
 		}
 		if(ev.x==4008)
 		{
-			if(cur>0)
+			if(i0>0)
 			{
-				cur--;
-				for(int i=cur;i<MAX_TEXT_LEN;i++)
+				i0--;
+				for(int i=i0;i<MAX_TEXT_LEN;i++)
 				{
 					text[i]=text[i+1];
 					if(text[i]==0)
@@ -132,7 +155,7 @@ int LineTextEvent(SButton& b, SScincEvent& ev)
 		}
 		if(ev.x==4011)
 		{
-			for(int i=cur;i<MAX_TEXT_LEN;i++)
+			for(int i=i0;i<MAX_TEXT_LEN;i++)
 			{
 				text[i]=text[i+1];
 				if(text[i]==0)
@@ -144,7 +167,7 @@ int LineTextEvent(SButton& b, SScincEvent& ev)
 		}
 		if(ev.x==4006)
 		{
-			cur=0;
+			i0=0;
 		}
 		if(ev.x==4007)
 		{
@@ -152,19 +175,19 @@ int LineTextEvent(SButton& b, SScincEvent& ev)
 			{
 				if(text[i]==0)
 				{
-					cur=i;
+					i0=i;
 					break;
 				}
 			}
 		}
 		if(ev.x==4013)
 		{
-			if(b.CB)
-				b.CB(&b);
+			if(b.actCB)
+				b.actCB(&b);
 		}
 	}
-	//printf(" Cur %i text after: <%s>\n", cur, text);
-	//b.cur=cur;
+	//printf(" Cur %i text after: <%s>\n", i0, text);
+	//b.i0=i0;
 	return 0;
 }
 
@@ -196,7 +219,7 @@ class CGUI
 	int prevmy;
 public:
 	int nb;
-	SButton buttons[64];
+	SWidget buttons[64];
 	int hoverButton;
 	int downButton;
 	void Init()
@@ -243,16 +266,16 @@ public:
 		hoverButton=-1;
 		if(downButton!=-1)
 		{
-			SButton& b=buttons[downButton];
+			SWidget& b=buttons[downButton];
 			if(b.type==CT_VSLIDE)
 			{
 				float v=(y-b.y0)/float(b.y1-b.y0-1);
 				if(v<0)v=0;
 				if(v>1)v=1;
 				*b.fDest=b.f0+(b.f1-b.f0)*v;
-				if(b.CB)
+				if(b.actCB)
 				{
-					b.CB(&b);
+					b.actCB(&b);
 				}
 			}
 			else if(b.type==CT_HSLIDE)
@@ -261,9 +284,9 @@ public:
 				if(v<0)v=0;
 				if(v>1)v=1;
 				*b.fDest=b.f0+(b.f1-b.f0)*v;
-				if(b.CB)
+				if(b.actCB)
 				{
-					b.CB(&b);
+					b.actCB(&b);
 				}
 			}
 		}
@@ -282,7 +305,7 @@ public:
 		GetMouseState(mx,my,mb);
 		if((downButton!=-1)&&(mb&2))
 		{
-			SButton& b=buttons[downButton];
+			SWidget& b=buttons[downButton];
 			if(KeyPressed(3000)||KeyPressed(3001))
 			{
 				b.x0+=x-prevmx;
@@ -305,28 +328,28 @@ public:
 		int ret=-1;
 		if((hoverButton!=-1)&&(downButton==hoverButton))
 		{
-			SButton& b=buttons[hoverButton];
+			SWidget& b=buttons[hoverButton];
 			if(b.type==CT_SELECT)
 			{
-				*b.iDest=b.cur;
-				if(b.CB)
+				*b.iDest=b.i0;
+				if(b.actCB)
 				{
-					ret=b.CB(&b);
+					ret=b.actCB(&b);
 				}
 			}
 			else if(b.type==CT_CHECK)
 			{
 				*b.iDest=!*b.iDest;
-				if(b.CB)
+				if(b.actCB)
 				{
-					ret=b.CB(&b);
+					ret=b.actCB(&b);
 				}
 			}
 			else if(b.type==CT_BUTTON)
 			{
-				if(b.CB)
+				if(b.actCB)
 				{
-					ret=b.CB(&b);
+					ret=b.actCB(&b);
 				}
 			}
 			downButton=-1;
@@ -352,147 +375,173 @@ public:
 			{
 				col=0xff405750;
 			}
-			SButton& b=buttons[i];
+			SWidget& b=buttons[i];
 			//g.Rect(b.x0, b.y0,b.x1-b.x0,b.y1-b.y0,0x30ff00ff);
 			//g.Rect(b.x0+1, b.y0+1,b.x1-b.x0-2,b.y1-b.y0-2,0x3000ff00);
 			g.Clip(b.x0, b.y0,b.x1-b.x0+1,b.y1-b.y0+1);
-			if(b.type==CT_BUTTON)
+			if(b.renderCB){}else // ! not work with pointers, TODO: fix
 			{
+				if(b.type==CT_BUTTON)
+				{
+					if(downButton==i)
+					{
+						g.Rect(b.x0, b.y0,b.x1-b.x0,b.y1-b.y0,0xff202720);
+						g.lineH(b.x0, b.y0,b.x1-b.x0,0xff202020);
+						g.lineH(b.x0, b.y1,b.x1-b.x0+1,0xff202020);
+						g.lineV(b.x0, b.y0,b.y1-b.y0,0xff202020);
+						g.lineV(b.x1, b.y0,b.y1-b.y0,0xff202020);
+					}
+					else if(hoverButton==i)
+					{
+						g.Rect(b.x0, b.y0,b.x1-b.x0,b.y1-b.y0,0xff405750);
+						g.lineH(b.x0, b.y0,b.x1-b.x0,0xffc0c0c0);
+						g.lineH(b.x0, b.y1,b.x1-b.x0+1,0xffc0c0c0);
+						g.lineV(b.x0, b.y0,b.y1-b.y0,0xffc0c0c0);
+						g.lineV(b.x1, b.y0,b.y1-b.y0,0xffc0c0c0);
+					}
+					else
+					{
+						g.Rect(b.x0, b.y0,b.x1-b.x0,b.y1-b.y0,0xff303740);
+						g.lineH(b.x0, b.y0,b.x1-b.x0,  0xff606060);
+						g.lineH(b.x0, b.y1,b.x1-b.x0+1,0xff606060);
+						g.lineV(b.x0, b.y0,b.y1-b.y0,  0xff606060);
+						g.lineV(b.x1, b.y0,b.y1-b.y0,  0xff606060);
+					}
+					int l=StrNLen(b.text,128);
+					stext(b.text, b.x0+(b.x1-b.x0-l*6)/2+2, b.y0+(b.y1-b.y0-11)/2+1,0x80000000);
+					stext(b.text, b.x0+(b.x1-b.x0-l*6)/2+1, b.y0+(b.y1-b.y0-11)/2,0xffffffff);
+					//stext(b.text, b.x0+10, b.y1-15,0xffffffff);
+				}
+				else if(b.type==CT_SELECT)
+				{
+					int h=b.y1-b.y0;
+					g.Circle((float)b.x0+9,(float)(b.y0+h/2),0,8,1,0xffc0c0c0);
+					g.Circle((float)b.x0+9,(float)(b.y0+h/2),8,0,1,col);
+					if(*b.iDest==b.i0)
+					{
+						g.Circle((float)b.x0+9  ,(float)b.y0+h/2,0,4,1,0xff000000);
+						g.Circle((float)b.x0+9-2,(float)b.y0+h/2-2,0,0,2,0xffffffff);
+					}
+					stext(b.text, b.x0+22, b.y1-h/2-5,0xffffffff);
+				}
+				else if(b.type==CT_CHECK)
+				{
+					int h=b.y1-b.y0;
+					g.Rect (b.x0+2, b.y0+h/2-9.5+2 ,16,16,0xffc0c0c0);
+					g.lineH(b.x0+1, b.y0+h/2-9.5+1 ,18,col);
+					g.lineH(b.x0+1, b.y0+h/2-9.5+18,18,col);
+					g.lineV(b.x0+1, b.y0+h/2-9.5+2 ,16,col);
+					g.lineV(b.x0+18,b.y0+h/2-9.5+2 ,16,col);
+					if(*b.iDest)
+					{
+						g.clear();
+						g.M((float)b.x0+4,(float)b.y0-1.5+h/2);
+						g.l(6.5,7);
+						g.l(11,-11);
+						g.l(0,1);
+						g.l(-11,15);
+						g.l(-6.5,-10.5);
+						g.close();
+						g.fin();
+						g.rgba32(0xff000000);
+						g.width(1,0.2);
+						g.fill2();
+						g.clear();
+						g.M((float)b.x0+3,(float)b.y0-4+h/2);
+						g.l(6.5,7);
+						g.l(11,-11);
+						g.l(0,1);
+						g.l(-11,15);
+						g.l(-6.5,-10.5);
+						g.close();
+						g.fin();
+						g.width(1,1);
+						g.rgba32(0xff206020);
+						g.stroke();
+						g.rgba32(0xff30c040);
+						g.fill1();
+					}
+					stext(b.text, b.x0+25, b.y1-h/2-5,0xffffffff);
+				}
+				else if(b.type==CT_VSLIDE)
+				{
+					g.lineH(b.x0, b.y0 ,b.x1-b.x0,col);
+					g.lineH(b.x0, b.y1-1,b.x1-b.x0,col);
+					g.lineV(b.x0, b.y0+1,b.y1-b.y0-2,col);
+					g.lineV(b.x1-1, b.y0+1,b.y1-b.y0-2,col);
+					int v=(1.-(*b.fDest-b.f0)/(b.f1-b.f0))*(b.y1-b.y0-2);
+					g.Rect(b.x0+1, b.y0+1, b.x1-b.x0-2,b.y1-b.y0-2,0xff404740);
+					g.Rect(b.x0+1, b.y1-v-1, b.x1-b.x0-2,v,0xff80b080);
+				}
+				else if(b.type==CT_HSLIDE)
+				{
+					g.lineH(b.x0, b.y0 ,b.x1-b.x0,col);
+					g.lineH(b.x0, b.y1-1,b.x1-b.x0,col);
+					g.lineV(b.x0, b.y0+1,b.y1-b.y0-2,col);
+					g.lineV(b.x1-1, b.y0+1,b.y1-b.y0-2,col);
+					int v=((*b.fDest-b.f0)/(b.f1-b.f0))*(b.x1-b.x0-2);
+					g.Rect(b.x0+1, b.y0+1, b.x1-b.x0-2,b.y1-b.y0-2,0xff404740);
+					g.Rect(b.x0+1, b.y0+1, v,b.y1-b.y0-2,0xff80b080);
+					char s[128];
+					int l=snprintf(s,128,"%s: %f", b.text, *b.fDest);
+					stext(s, b.x0+(b.x1-b.x0-l*6)/2, b.y0+(b.y1-b.y0-11)/2,0xffffffff);
+				}
+				else if(b.type==CT_STATIC)
+				{
+					int l=StrNLen(b.text,128);
+					stext(b.text, b.x0+(b.x1-b.x0-l*6)/2+1, b.y0+(b.y1-b.y0-11)/2,0xffffffff);
+				}
+				else if(b.type==CT_FRAME)
+				{
+					g.Rect(b.x0+1, b.y0+1, b.x1-b.x0-2,b.y1-b.y0-2,0xff404740);
+				}
+				else if(b.type==CT_LINEEDIT)
+				{
+					int x=b.x0;
+					int y=b.y0;
+					int w=b.x1-x;
+					int h=b.y1-y;
+					int curPos=x+b.i0*9+2;
+					int offset=0;
+					if(curPos>w-11)
+					{
+						offset=curPos-(w-11);
+						curPos=w-11;
+					}
+					g.Rect(x+1,y+1, w-2, h-2, 0xfff0e0d0);
+					g.rgba32(0xff404040);
+					g.lineV(x,y,h);
+					g.lineH(x+1,y,w-2);
+					g.rgba32(0xffc0c0c0);
+					g.lineH(x+1,y+h-1,w-2);
+					g.lineV(x+w-1,y,h);
+					stexts(b.text, x+2-offset, y+h-16, 0xff000000,1);
+					if(int(Time()*2)%2)
+						g.Rect(curPos,y+h-18,2,17,0xff008000);
+					else
+						g.Rect(curPos,y+h-18,2,17,0xffff80ff);
+				}
+			}
+			if(b.renderCB)
+			{
+				int state=0;
 				if(downButton==i)
 				{
-					g.Rect(b.x0, b.y0,b.x1-b.x0,b.y1-b.y0,0xff202720);
-					g.lineH(b.x0, b.y0,b.x1-b.x0,0xff202020);
-					g.lineH(b.x0, b.y1,b.x1-b.x0+1,0xff202020);
-					g.lineV(b.x0, b.y0,b.y1-b.y0,0xff202020);
-					g.lineV(b.x1, b.y0,b.y1-b.y0,0xff202020);
+					state|=1;
 				}
 				else if(hoverButton==i)
 				{
-					g.Rect(b.x0, b.y0,b.x1-b.x0,b.y1-b.y0,0xff405750);
-					g.lineH(b.x0, b.y0,b.x1-b.x0,0xffc0c0c0);
-					g.lineH(b.x0, b.y1,b.x1-b.x0+1,0xffc0c0c0);
-					g.lineV(b.x0, b.y0,b.y1-b.y0,0xffc0c0c0);
-					g.lineV(b.x1, b.y0,b.y1-b.y0,0xffc0c0c0);
+					state|=2;
 				}
-				else
-				{
-					g.Rect(b.x0, b.y0,b.x1-b.x0,b.y1-b.y0,0xff303740);
-					g.lineH(b.x0, b.y0,b.x1-b.x0,  0xff606060);
-					g.lineH(b.x0, b.y1,b.x1-b.x0+1,0xff606060);
-					g.lineV(b.x0, b.y0,b.y1-b.y0,  0xff606060);
-					g.lineV(b.x1, b.y0,b.y1-b.y0,  0xff606060);
-				}
-				int l=StrNLen(b.text,128);
-				stext(b.text, b.x0+(b.x1-b.x0-l*6)/2+2, b.y0+(b.y1-b.y0-11)/2+1,0x80000000);
-				stext(b.text, b.x0+(b.x1-b.x0-l*6)/2+1, b.y0+(b.y1-b.y0-11)/2,0xffffffff);
-				//stext(b.text, b.x0+10, b.y1-15,0xffffffff);
+				b.renderCB(&b, state);
+				g.UnClip();
+				continue;
 			}
-			else if(b.type==CT_SELECT)
-			{
-				int h=b.y1-b.y0;
-				g.Circle((float)b.x0+9,(float)(b.y0+h/2),0,8,1,0xffc0c0c0);
-				g.Circle((float)b.x0+9,(float)(b.y0+h/2),8,0,1,col);
-				if(*b.iDest==b.cur)
-				{
-					g.Circle((float)b.x0+9  ,(float)b.y0+h/2,0,4,1,0xff000000);
-					g.Circle((float)b.x0+9-2,(float)b.y0+h/2-2,0,0,2,0xffffffff);
-				}
-				stext(b.text, b.x0+22, b.y1-h/2-5,0xffffffff);
-			}
-			else if(b.type==CT_CHECK)
-			{
-				int h=b.y1-b.y0;
-				g.Rect (b.x0+2, b.y0+h/2-9.5+2 ,16,16,0xffc0c0c0);
-				g.lineH(b.x0+1, b.y0+h/2-9.5+1 ,18,col);
-				g.lineH(b.x0+1, b.y0+h/2-9.5+18,18,col);
-				g.lineV(b.x0+1, b.y0+h/2-9.5+2 ,16,col);
-				g.lineV(b.x0+18,b.y0+h/2-9.5+2 ,16,col);
-				if(*b.iDest)
-				{
-					g.clear();
-					g.M((float)b.x0+4,(float)b.y0-1.5+h/2);
-					g.l(6.5,7);
-					g.l(11,-11);
-					g.l(0,1);
-					g.l(-11,15);
-					g.l(-6.5,-10.5);
-					g.close();
-					g.fin();
-					g.rgba32(0xff000000);
-					g.width(1,0.2);
-					g.fill2();
-					g.clear();
-					g.M((float)b.x0+3,(float)b.y0-4+h/2);
-					g.l(6.5,7);
-					g.l(11,-11);
-					g.l(0,1);
-					g.l(-11,15);
-					g.l(-6.5,-10.5);
-					g.close();
-					g.fin();
-					g.width(1,1);
-					g.rgba32(0xff206020);
-					g.stroke();
-					g.rgba32(0xff30c040);
-					g.fill1();
-				}
-				stext(b.text, b.x0+25, b.y1-h/2-5,0xffffffff);
-			}
-			else if(b.type==CT_VSLIDE)
-			{
-				g.lineH(b.x0, b.y0 ,b.x1-b.x0,col);
-				g.lineH(b.x0, b.y1-1,b.x1-b.x0,col);
-				g.lineV(b.x0, b.y0+1,b.y1-b.y0-2,col);
-				g.lineV(b.x1-1, b.y0+1,b.y1-b.y0-2,col);
-				int v=(1.-(*b.fDest-b.f0)/(b.f1-b.f0))*(b.y1-b.y0-2);
-				g.Rect(b.x0+1, b.y0+1, b.x1-b.x0-2,b.y1-b.y0-2,0xff404740);
-				g.Rect(b.x0+1, b.y1-v-1, b.x1-b.x0-2,v,0xff80b080);
-			}
-			else if(b.type==CT_HSLIDE)
-			{
-				g.lineH(b.x0, b.y0 ,b.x1-b.x0,col);
-				g.lineH(b.x0, b.y1-1,b.x1-b.x0,col);
-				g.lineV(b.x0, b.y0+1,b.y1-b.y0-2,col);
-				g.lineV(b.x1-1, b.y0+1,b.y1-b.y0-2,col);
-				int v=((*b.fDest-b.f0)/(b.f1-b.f0))*(b.x1-b.x0-2);
-				g.Rect(b.x0+1, b.y0+1, b.x1-b.x0-2,b.y1-b.y0-2,0xff404740);
-				g.Rect(b.x0+1, b.y0+1, v,b.y1-b.y0-2,0xff80b080);
-				char s[128];
-				int l=snprintf(s,128,"%s: %f", b.text, *b.fDest);
-				stext(s, b.x0+(b.x1-b.x0-l*6)/2, b.y0+(b.y1-b.y0-11)/2,0xffffffff);
-			}
-			else if(b.type==CT_STATIC)
-			{
-				int l=StrNLen(b.text,128);
-				stext(b.text, b.x0+(b.x1-b.x0-l*6)/2+1, b.y0+(b.y1-b.y0-11)/2,0xffffffff);
-			}
-			else if(b.type==CT_FRAME)
-			{
-				g.Rect(b.x0+1, b.y0+1, b.x1-b.x0-2,b.y1-b.y0-2,0xff404740);
-			}
-			else if(b.type==CT_LINEEDIT)
-			{
-				int x=b.x0;
-				int y=b.y0;
-				int w=b.x1-x;
-				int h=b.y1-y;
-				g.Rect(x+1,y+1, w-2, h-2, 0xfff0e0d0);
-				g.rgba32(0xff404040);
-				g.lineV(x,y,h);
-				g.lineH(x+1,y,w-2);
-				g.rgba32(0xffc0c0c0);
-				g.lineH(x+1,y+h-1,w-2);
-				g.lineV(x+w-1,y,h);
-				stexts(b.text, x+2, y+h-16, 0xff000000,1);
-				if(int(Time()*2)%2)
-					g.Rect(x+b.cur*9+2,y+h-18,2,17,0xff008000);
-				else
-					g.Rect(x+b.cur*9+2,y+h-18,2,17,0xffff80ff);
-			}
+
 			g.UnClip();
 		}
 	}
-	SButton& AddButton(char* text, int x, int y, int w, int h, int tag)
+	SWidget& AddButton(char* text, int x, int y, int w, int h, int tag)
 	{
 		buttons[nb].type=CT_BUTTON;
 		buttons[nb].x0=x;
@@ -500,12 +549,13 @@ public:
 		buttons[nb].x1=x+w;
 		buttons[nb].y1=y+h;
 		buttons[nb].tag=tag;
-		buttons[nb].CB=0;
+		buttons[nb].actCB=0;
+		buttons[nb].renderCB=0;
 		buttons[nb].text=text;
 		nb++;
 		return buttons[nb-1];
 	}
-	SButton& AddCheck(char* text, int x, int y, int w, int h, int tag, int* dest)
+	SWidget& AddCheck(char* text, int x, int y, int w, int h, int tag, int* dest)
 	{
 		buttons[nb].type=CT_CHECK;
 		buttons[nb].text=text;
@@ -515,11 +565,12 @@ public:
 		buttons[nb].y1=y+h;
 		buttons[nb].tag=tag;
 		buttons[nb].iDest=dest;
-		buttons[nb].CB=0;
+		buttons[nb].actCB=0;
+		buttons[nb].renderCB=0;
 		nb++;
 		return buttons[nb-1];
 	}
-	SButton& AddSelect(char* text, int x, int y, int w, int h, int tag, int value, int* dest)
+	SWidget& AddSelect(char* text, int x, int y, int w, int h, int tag, int value, int* dest)
 	{
 		buttons[nb].type=CT_SELECT;
 		buttons[nb].text=text;
@@ -528,13 +579,14 @@ public:
 		buttons[nb].x1=x+w;
 		buttons[nb].y1=y+h;
 		buttons[nb].tag=tag;
-		buttons[nb].cur=value;
+		buttons[nb].i0=value;
 		buttons[nb].iDest=dest;
-		buttons[nb].CB=0;
+		buttons[nb].actCB=0;
+		buttons[nb].renderCB=0;
 		nb++;
 		return buttons[nb-1];
 	}
-	SButton& AddSlide(char* text, int x, int y, int w, int h, int tag, float* dest, bool vert, float f0, float f1)
+	SWidget& AddSlide(char* text, int x, int y, int w, int h, int tag, float* dest, bool vert, float f0, float f1)
 	{
 		buttons[nb].type=vert?CT_VSLIDE:CT_HSLIDE;
 		buttons[nb].text=text;
@@ -546,11 +598,12 @@ public:
 		buttons[nb].fDest=dest;
 		buttons[nb].f0=f0;
 		buttons[nb].f1=f1;
-		buttons[nb].CB=0;
+		buttons[nb].actCB=0;
+		buttons[nb].renderCB=0;
 		nb++;
 		return buttons[nb-1];
 	}
-	SButton& AddStatic(char* text, int x, int y, int w, int h)
+	SWidget& AddStatic(char* text, int x, int y, int w, int h)
 	{
 		buttons[nb].type=CT_STATIC;
 		buttons[nb].text=text;
@@ -558,11 +611,12 @@ public:
 		buttons[nb].y0=y;
 		buttons[nb].x1=x+w;
 		buttons[nb].y1=y+h;
-		buttons[nb].CB=0;
+		buttons[nb].actCB=0;
+		buttons[nb].renderCB=0;
 		nb++;
 		return buttons[nb-1];
 	}
-	SButton& AddFrame(char* text, int x, int y, int w, int h)
+	SWidget& AddFrame(char* text, int x, int y, int w, int h)
 	{
 		buttons[nb].type=CT_FRAME;
 		buttons[nb].text=text;
@@ -570,11 +624,12 @@ public:
 		buttons[nb].y0=y;
 		buttons[nb].x1=x+w;
 		buttons[nb].y1=y+h;
-		buttons[nb].CB=0;
+		buttons[nb].actCB=0;
+		buttons[nb].renderCB=0;
 		nb++;
 		return buttons[nb-1];
 	}
-	SButton& AddLineEdit(char* text, int x, int y, int w, int h)
+	SWidget& AddLineEdit(char* text, int x, int y, int w, int h)
 	{
 		buttons[nb].type=CT_LINEEDIT;
 		buttons[nb].text=text;
@@ -582,8 +637,9 @@ public:
 		buttons[nb].y0=y;
 		buttons[nb].x1=x+w;
 		buttons[nb].y1=y+h;
-		buttons[nb].CB=0;
-		buttons[nb].cur=0;
+		buttons[nb].actCB=0;
+		buttons[nb].renderCB=0;
+		buttons[nb].i0=0;
 		nb++;
 		return buttons[nb-1];
 	}
--- examples/ui/gui/TestGUI.cpp
+++ examples/ui/gui/TestGUI.cpp
@@ -16,9 +16,9 @@ int main()
 	int ch1=1;
 	float val[5]={.5,.3,.7,0.,1.};
 
-	gui.AddButton("Go", 20,20,30,20,'go__').SetCB([](SButton*p)->int
+	gui.AddButton("Go", 20,20,30,20,'go__').SetActionCB([](SWidget*p)->int
 	{
-		SButton& self=*p;
+		SWidget& self=*p;
 		if(*(self.text)=='G')
 		{
 			self.text="Stop";
@@ -34,20 +34,20 @@ int main()
 		return 1;
 	}).SetTitle("Stop");
 
-	gui.AddSelect("Select A", 20,50,80,20,'sel0',1,&sel).SetCB([](SButton*p)->int{p->text="002";return 0;});
+	gui.AddSelect("Select A", 20,50,80,20,'sel0',1,&sel).SetActionCB([](SWidget*p)->int{p->text="002";return 0;});
 	gui.AddSelect("Select B", 20,80,80,20,'sel1',2,&sel);
-	gui.AddCheck("Check 0", 20,110,80,20,'ch_0',&ch0).SetCB([](SButton*p)->int{p->text="001";return 0;});
+	gui.AddCheck("Check 0", 20,110,80,20,'ch_0',&ch0).SetActionCB([](SWidget*p)->int{p->text="001";return 0;});
 	gui.AddCheck("Check 1", 20,140,80,20,'ch_1',&ch1);
 	gui.AddSlide("val0", 20,170,201,15,'val0',&val[0],false,0,100);
 	gui.AddFrame("", 120,10,95,130);
 	gui.AddStatic("Values", 140,10,50,20);
-	gui.AddSlide("", 130,30,15,100,'val1',&val[1],true,0,1).SetCB([](SButton*p)->int{printf("%f\n",*p->fDest);return 0;});
+	gui.AddSlide("", 130,30,15,100,'val1',&val[1],true,0,1).SetActionCB([](SWidget*p)->int{printf("%f\n",*p->fDest);return 0;});
 	gui.AddSlide("", 150,30,15,100,'val2',&val[2],true,0,1);
 	gui.AddSlide("", 170,30,15,100,'val3',&val[3],true,0,1);
 	gui.AddSlide("", 190,30,15,100,'val4',&val[4],true,0,1);
 	char editLine[MAX_TEXT_LEN]={};
 	gui.AddStatic(editLine, 20,210,100,20);
-	gui.AddLineEdit(editLine, 20,190,100,20).SetCB([](SButton*p)->int{printf("'%s'\n",p->text);return 0;});
+	gui.AddLineEdit(editLine, 20,190,100,20).SetActionCB([](SWidget*p)->int{printf("'%s'\n",p->text);return 0;});
 
 	SetPresentWait(true);
 
